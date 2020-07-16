c48727f6e1a9145dddda2f8542f93753ac21f2e4
Harsimrat Sandhwalia <hs.sandhawalia@gmail.com>
2020-07-16 11:13:01 +0200

    added example for cli

---
+++ oatomobile/examples/priveleged-agent.py
@@ -0,0 +1,19 @@
+import oatomobile
+
+# Initializes a CARLA environment.
+environment = oatomobile.envs.CARLAEnv(town="Town01")
+
+# Makes an initial observation.
+observation = environment.reset()
+done = False
+
+while not done:
+  # Selects a random action.
+  action = environment.action_space.sample()
+  observation, reward, done, info = environment.step(action)
+
+  # Renders interactive display.
+  environment.render(mode="human")
+
+# Book-keeping: closes
+environment.close()
