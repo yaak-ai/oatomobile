5950f0463b4a7ad9f95478b0086bd42e8e4bfc2f
Harsimrat Sandhwalia <hs.sandhawalia@gmail.com>
2020-07-18 10:21:16 +0200

    Carl visualization

--- oatomobile/examples/priveleged-agent.py
+++ oatomobile/examples/priveleged-agent.py
@@ -1,19 +1,39 @@
+import argparse
+
+import gym
 import oatomobile
 
-# Initializes a CARLA environment.
-environment = oatomobile.envs.CARLAEnv(town="Town01")
 
-# Makes an initial observation.
-observation = environment.reset()
-done = False
+def run_rendrer(town, save_path, interactive):
+  # Initializes a CARLA environment.
+  env = oatomobile.envs.CARLAEnv(town=town)
+  env = gym.wrappers.Monitor(env, save_path)
 
-while not done:
-  # Selects a random action.
-  action = environment.action_space.sample()
-  observation, reward, done, info = environment.step(action)
+  # Makes an initial observation.
+  obs = env.reset()
+  done = False
 
-  # Renders interactive display.
-  environment.render(mode="human")
+  while not done:
+    # Selects a random action.
+    action = env.action_space.sample()
+    observation, reward, done, info = env.step(action)
 
-# Book-keeping: closes
-environment.close()
+    # Renders interactive display.
+    if interactive:
+      env.render(mode="human")
+
+  # Book-keeping: closes
+  env.close()
+
+
+if __name__ == '__main__':
+
+  parser = argparse.ArgumentParser("Remote render and saving script")
+  parser.add_argument('-v', '--vid', help='Path where to save the videos')
+  parser.add_argument('-t', '--town', help='Town Name')
+  parser.add_argument('-i', '--interactive', action='store_true', default=False,
+                      help='Interactive visualization')
+
+  args = parser.parse_args()
+
+  run_rendrer(args.town, args.vid, args.interactive)
